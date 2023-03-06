b4e41dba2248c6bae61cc318abd00e8104438173
nstonic <118763806+nstonic@users.noreply.github.com>
2023-03-06 23:08:08 +0400

    Add start arguments

--- render_website.py
+++ render_website.py
@@ -1,12 +1,16 @@
 import json
 import os
 
+from argparse import ArgumentParser
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
+from pathvalidate.argparse import validate_filepath_arg
+
+from settings import JSON_PATH
 
 
-def render_page():
+def render_page(json_path: str):
     env = Environment(
         loader=FileSystemLoader(''),
         autoescape=select_autoescape(['html', 'xml'])
@@ -14,26 +18,45 @@ def render_page():
 
     template = env.get_template('template.html')
 
-    with open('books.json') as file:
-        books = json.load(file)
-    books_on_page = 10
-    books_by_pages = list(chunked(books, books_on_page))
-    for index, books in enumerate(books_by_pages):
-        page_number = index + 1
+    try:
+        with open(json_path) as file:
+            books_cards = json.load(file)
+    except FileNotFoundError:
+        print(
+            'Необходимо указать путь к json-файлу с данными либо в файле settings.py, '
+            'либо в аргументе --json_path при запуске скрипта из командной строки.'
+            'Подробнее читайте в README.'
+        )
+        exit()
+    book_cards_on_page = 10
+    book_cards_separated_by_pages = list(chunked(books_cards, book_cards_on_page))
+    for page_number, books_cards in enumerate(book_cards_separated_by_pages, start=1):
         number_of_columns = 2
         rendered_page = template.render(
-            books=chunked(books, number_of_columns),
-            total_pages=len(books_by_pages),
+            books=chunked(books_cards, number_of_columns),
+            total_pages=len(book_cards_separated_by_pages),
             current_page=page_number
         )
         os.makedirs('pages', exist_ok=True)
         page_path = os.path.join('pages', f'index{page_number}.html')
-        with open(page_path, 'w', encoding="utf8") as file:
+        with open(page_path, 'w', encoding='utf8') as file:
             file.write(rendered_page)
 
 
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--json_path',
+        type=validate_filepath_arg,
+        help='Указать путь к json-файлу с данными'
+    )
+    args = parser.parse_args()
+    json_path = args.json_path or JSON_PATH
+    render_page(json_path)
+
+
 if __name__ == '__main__':
-    render_page()
+    main()
     server = Server()
-    server.watch(os.path.join('template.html'), render_page)
+    server.watch(os.path.join('template.html'), main)
     server.serve()
